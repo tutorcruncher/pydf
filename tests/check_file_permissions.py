668d08f304ae901d0b02358eb3cc581befcac7a0
Samuel Colvin <s@muelcolvin.com>
2016-01-13 14:35:26 +0000

    better file permissions checks

--- tests/check_file_permissions.py
+++ tests/check_file_permissions.py
@@ -2,11 +2,17 @@
 import os
 import stat
 
-for root, dirs, files in os.walk('env'):
-    if root.endswith('/pydf/bin'):
-        assert 'wkhtmltopdf' in files, '"wkhtmltopdf" not in files %r' % files
+found = False
+for root, dirs, files in os.walk('/'):
+    if root.endswith('/pydf/bin') and files == ['wkhtmltopdf']:
         assert len(files) == 1, 'more than file: %r' % files
         path = os.path.join(root, files[0])
         st = os.stat(path)
         perms = oct(st[stat.ST_MODE])[-3:]
         assert perms == '775', 'wrong file permissions for "%s": "%s"' % (path, perms)
+        print('"%s" has correct permissions 775' % path)
+        found = True
+        break
+
+if not found:
+    raise Exception('wkhtmltopdf binary not found')
