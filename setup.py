e2b606658eac939c091ef9c02357eca3ee12a15c
Tom Hamilton Stubber <tomhamiltonstubber@gmail.com>
2024-04-03 17:58:10 +0100

    Uprev to python3.11

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Internet :: WWW/HTTP',
         ],
     zip_safe=False
